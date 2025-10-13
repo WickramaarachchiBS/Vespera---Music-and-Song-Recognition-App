034a0ded865973c23fb7c842abf6a0107fdab06a
Wickramaarachchi B S <132426591+WickramaarachchiBS@users.noreply.github.com>
2025-10-14 00:28:43 +0530

    Upadated home page but must refactor and edited

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,52 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Home Screen')), body: const Center(child: Text('Welcome to the Home Screen!')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        centerTitle: true,
+        leading: Image(image: AssetImage('assetName')),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(children: [Row(), Row(), Row()]),
+            Column(children: [Row(), Row(), Row()]),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.library_music), label: 'Library'),
+          BottomNavigationBarItem(icon: Icon(Icons.record_voice_over_rounded), label: 'Whisper'),
+        ],
+        currentIndex: 0,
+        onTap: (index) {
+          // Handle navigation based on the tapped index
+          switch (index) {
+            case 0:
+              Navigator.pushNamed(context, '/');
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/search');
+              break;
+            case 2:
+              Navigator.pushNamed(context, '/library');
+              break;
+            case 3:
+              Navigator.pushNamed(context, '/library');
+              break;
+          }
+        },
+      ),
+    );
   }
 }
